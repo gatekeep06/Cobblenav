0395e1b0a8470cb47c9a58456beb2157e9445427
gatekeep06 <mvmetacontent@gmail.com>
2024-04-11 21:13:49 +0300

    fix clearContacts command

--- src/main/java/com/metacontent/cobblenav/command/ClearContactDataCommand.java
+++ src/main/java/com/metacontent/cobblenav/command/ClearContactDataCommand.java
@@ -1,5 +1,6 @@
 package com.metacontent.cobblenav.command;
 
+import com.metacontent.cobblenav.Cobblenav;
 import com.metacontent.cobblenav.util.ContactSaverEntity;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
@@ -12,6 +13,7 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
@@ -23,20 +25,26 @@ public class ClearContactDataCommand {
                                 .executes(ClearContactDataCommand::run))));
     }
 
-    private static int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        List<ServerPlayerEntity> players = context.getArgument("player", EntitySelector.class).getPlayers(context.getSource());
-        if (!players.isEmpty()) {
-            players.forEach(player -> {
-                if (player instanceof ContactSaverEntity contactSaverEntity) {
-                    NbtCompound nbt = contactSaverEntity.cobblenav$getContactData();
-                    nbt.getKeys().forEach(key -> {
-                        if (!Objects.equals(key, "title")) {
-                            nbt.remove(key);
+    private static int run(CommandContext<ServerCommandSource> context) {
+        try {
+            List<ServerPlayerEntity> players = context.getArgument("player", EntitySelector.class).getPlayers(context.getSource());
+            if (!players.isEmpty()) {
+                players.forEach(player -> {
+                    if (player instanceof ContactSaverEntity contactSaverEntity) {
+                        NbtCompound nbt = contactSaverEntity.cobblenav$getContactData();
+                        for (Iterator<String> iterator = nbt.getKeys().iterator(); iterator.hasNext(); ) {
+                            String key = iterator.next();
+                            if (!key.equals("title")) {
+                                nbt.remove(key);
+                            }
                         }
-                    });
-                }
-            });
-            return 1;
+                    }
+                });
+                return 1;
+            }
+        }
+        catch (Throwable e) {
+            Cobblenav.LOGGER.error(e.getMessage(), e);
         }
         return -1;
     }
