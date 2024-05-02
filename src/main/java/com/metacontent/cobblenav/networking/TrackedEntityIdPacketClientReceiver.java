fea8e9f3182d5a397f38724b3877ec34395d1600
gatekeep06 <mvmetacontent@gmail.com>
2024-05-02 12:45:07 +0300

    add config paremeter to allow notifying

--- src/main/java/com/metacontent/cobblenav/networking/TrackedEntityIdPacketClientReceiver.java
+++ src/main/java/com/metacontent/cobblenav/networking/TrackedEntityIdPacketClientReceiver.java
@@ -12,7 +12,7 @@ public class TrackedEntityIdPacketClientReceiver {
     public static void receive(MinecraftClient client, ClientPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender) {
         int id = buf.readInt();
         if (id == -1) {
-            if (client.player != null) {
+            if (client.player != null && CobblenavClient.CONFIG.notifyIfPokemonIsNotFound) {
                 client.player.sendMessage(Text.translatable("message.cobblenav.not_found_message")
                         .setStyle(Style.EMPTY.withItalic(true).withColor(0xff9a38)));
             }
