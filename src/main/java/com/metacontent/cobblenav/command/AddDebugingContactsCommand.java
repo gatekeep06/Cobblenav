2aaa1a09aa729ca05bdc75bbd910b06d2cbb106d
gatekeep06 <mvmetacontent@gmail.com>
2024-05-20 23:36:59 +0300

    complete contact debuging command

--- src/main/java/com/metacontent/cobblenav/command/AddDebugingContactsCommand.java
+++ src/main/java/com/metacontent/cobblenav/command/AddDebugingContactsCommand.java
@@ -2,6 +2,7 @@ package com.metacontent.cobblenav.command;
 
 import com.metacontent.cobblenav.store.ContactData;
 import com.metacontent.cobblenav.util.PokenavContact;
+import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
@@ -10,6 +11,8 @@ import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+
+import java.util.UUID;
 
 public class AddDebugingContactsCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess commandRegistryAccess, CommandManager.RegistrationEnvironment registrationEnvironment) {
@@ -23,7 +26,9 @@ public class AddDebugingContactsCommand {
         int amount = context.getArgument("amount", Integer.class);
         ContactData.executeForDataOf(player, contactData -> {
             for (int i = 0; i < amount; i++) {
-
+                GameProfile profile = new GameProfile(UUID.randomUUID(), "Player" + i);
+                PokenavContact contact = new PokenavContact(profile.getId().toString(), profile);
+                contactData.getContacts().put(contact.getKey(), contact);
             }
         });
         return 1;
