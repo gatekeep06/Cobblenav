9fc83cd272f56c39742c57391b9622d352dc20f5
gatekeep06 <mvmetacontent@gmail.com>
2024-05-22 21:36:29 +0300

    optimize imports

--- src/main/java/com/metacontent/cobblenav/mixin/CobblenavMixinConfigPlugin.java
+++ src/main/java/com/metacontent/cobblenav/mixin/CobblenavMixinConfigPlugin.java
@@ -1,6 +1,5 @@
 package com.metacontent.cobblenav.mixin;
 
-import com.metacontent.cobblenav.Cobblenav;
 import net.fabricmc.loader.api.FabricLoader;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
