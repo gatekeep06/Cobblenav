9c118c44a3fd3b8e6620289dac914d8647597ce3
gatekeep06 <mvmetacontent@gmail.com>
2024-05-23 11:48:57 +0300

    add receivers' functionality

--- src/main/java/com/metacontent/cobblenav/networking/PlayerStatsReceiver.java
+++ src/main/java/com/metacontent/cobblenav/networking/PlayerStatsReceiver.java
@@ -1,5 +1,7 @@
 package com.metacontent.cobblenav.networking;
 
+import com.metacontent.cobblenav.client.screen.pokenav.StatsScreen;
+import com.metacontent.cobblenav.util.PlayerStats;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
@@ -7,6 +9,9 @@ import net.minecraft.network.PacketByteBuf;
 
 public class PlayerStatsReceiver {
     public static void receive(MinecraftClient client, ClientPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender) {
+        PlayerStats playerStats = PlayerStats.fromBuf(buf);
+        if (client.currentScreen instanceof StatsScreen statsScreen) {
 
+        }
     }
 }
