b64c0a1267b76f6da57486a48f2610361faab9f7
gatekeep06 <mvmetacontent@gmail.com>
2024-05-26 00:49:17 +0300

    make crawling line widget extend AbstractTextWidget

--- src/main/java/com/metacontent/cobblenav/client/widget/CrawlingLineWidget.java
+++ src/main/java/com/metacontent/cobblenav/client/widget/CrawlingLineWidget.java
@@ -5,18 +5,14 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.widget.AbstractTextWidget;
 import net.minecraft.text.Text;
 
-public class CrawlingLineWidget implements Drawable {
+public class CrawlingLineWidget extends AbstractTextWidget {
     private static final int DELAY = 40;
     private float delayed = 0f;
-    private final TextRenderer textRenderer;
     private Text text;
     private final boolean shadow;
-    private int x;
-    private int y;
-    private int width;
-    private int height;
     private final float scale;
     private final BorderBox textOffsets;
     private int textWidth;
@@ -24,12 +20,8 @@ public class CrawlingLineWidget implements Drawable {
     private boolean shouldMoveToLeft = true;
 
     public CrawlingLineWidget(Text text, int x, int y, int width, int height, float scale, BorderBox textOffsets, boolean shadow) {
-        this.textRenderer = MinecraftClient.getInstance().textRenderer;
+        super(x, y, width, height, text, MinecraftClient.getInstance().textRenderer);
         setText(text);
-        setX(x);
-        this.y = y;
-        this.width = width;
-        this.height = height;
         this.scale = scale;
         this.textOffsets = textOffsets;
         this.shadow = shadow;
@@ -41,26 +33,26 @@ public class CrawlingLineWidget implements Drawable {
 
     public void setText(Text text) {
         this.text = text;
-        this.textWidth = textRenderer.getWidth(text);
+        this.textWidth = getTextRenderer().getWidth(text);
     }
 
     @Override
-    public void render(DrawContext drawContext, int i, int j, float f) {
-        drawContext.enableScissor(x, y, x + width, y + height);
+    protected void renderButton(DrawContext drawContext, int i, int j, float f) {
+        drawContext.enableScissor(getX(), getY(), getX() + width, getY() + height);
         drawContext.getMatrices().push();
         drawContext.getMatrices().scale(scale, scale, 1f);
-        drawContext.drawText(textRenderer, text, (int) (textX / scale) + textOffsets.left, (int) (y / scale) + textOffsets.top, 0xffffff, shadow);
+        drawContext.drawText(getTextRenderer(), text, (int) (textX / scale) + textOffsets.left, (int) (getY() / scale) + textOffsets.top, 0xffffff, shadow);
         drawContext.getMatrices().pop();
         drawContext.disableScissor();
         crawl(f);
     }
 
     public void renderDynamic(DrawContext drawContext, Text text, boolean shadow, float f) {
-        this.textWidth = (int) (textRenderer.getWidth(text) * scale);
-        drawContext.enableScissor(x, y, x + width, y + height);
+        this.textWidth = (int) (getTextRenderer().getWidth(text) * scale);
+        drawContext.enableScissor(getX(), getY(), getX() + width, getY() + height);
         drawContext.getMatrices().push();
         drawContext.getMatrices().scale(scale, scale, 1f);
-        drawContext.drawText(textRenderer, text, (int) (textX / scale) + textOffsets.left, (int) (y / scale) + textOffsets.top, 0xffffff, shadow);
+        drawContext.drawText(getTextRenderer(), text, (int) (textX / scale) + textOffsets.left, (int) (getY() / scale) + textOffsets.top, 0xffffff, shadow);
         drawContext.getMatrices().pop();
         drawContext.disableScissor();
         crawl(f);
@@ -68,7 +60,7 @@ public class CrawlingLineWidget implements Drawable {
 
     private void crawl(float f) {
         if (textWidth <= width) {
-            textX = x;
+            textX = getX();
             return;
         }
         if (delayed >= DELAY) {
@@ -78,7 +70,7 @@ public class CrawlingLineWidget implements Drawable {
             else {
                 textX += 0.1f;
             }
-            if (textX >= (float) x || textX <= (float) (x - (textWidth - width))) {
+            if (textX >= (float) getX() || textX <= (float) (getX() - (textWidth - width))) {
                 delayed = 0f;
                 shouldMoveToLeft = !shouldMoveToLeft;
             }
@@ -88,12 +80,9 @@ public class CrawlingLineWidget implements Drawable {
         }
     }
 
-    public void setX(int x) {
-        this.x = x;
-        this.textX = (float) x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
+    @Override
+    public void setX(int i) {
+        super.setX(i);
+        this.textX = (float) getX();
     }
 }
