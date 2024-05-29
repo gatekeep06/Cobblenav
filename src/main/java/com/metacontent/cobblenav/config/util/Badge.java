ae468587314e1bc45dd71b59956bb120c484db08
gatekeep06 <mvmetacontent@gmail.com>
2024-05-29 12:21:21 +0300

    add method to check permission to grant

--- src/main/java/com/metacontent/cobblenav/config/util/Badge.java
+++ src/main/java/com/metacontent/cobblenav/config/util/Badge.java
@@ -1,7 +1,13 @@
 package com.metacontent.cobblenav.config.util;
 
+import me.lucko.fabric.api.permissions.v0.Permissions;
+import net.minecraft.server.network.ServerPlayerEntity;
+
 public record Badge(
         String type,
         String permissionToGrant
 ) {
+    public boolean hasPermissionToGrant(ServerPlayerEntity player) {
+        return Permissions.check(player, permissionToGrant);
+    }
 }
