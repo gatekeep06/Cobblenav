7812834c6a7374beebb9f39b1c9800d526c84ef2
gatekeep06 <mvmetacontent@gmail.com>
2024-06-08 08:42:34 +0300

    add abstract parent class for pokemon finders

---
+++ src/main/java/com/metacontent/cobblenav/util/finder/PokemonFinder.java
@@ -0,0 +1,35 @@
+package com.metacontent.cobblenav.util.finder;
+
+import com.cobblemon.mod.common.entity.pokemon.PokemonEntity;
+import com.metacontent.cobblenav.Cobblenav;
+import com.metacontent.cobblenav.util.FoundPokemon;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Box;
+
+import java.util.List;
+
+public abstract class PokemonFinder {
+    protected final PlayerEntity player;
+    protected final ServerWorld world;
+
+    public PokemonFinder(PlayerEntity player, ServerWorld world) {
+        this.player = player;
+        this.world = world;
+    }
+
+    public List<PokemonEntity> find(String name) {
+        return world.getEntitiesByClass(
+                PokemonEntity.class,
+                Box.of(
+                        player.getPos(),
+                        Cobblenav.CONFIG.findingAreaWidth,
+                        Cobblenav.CONFIG.findingAreaHeight,
+                        Cobblenav.CONFIG.findingAreaWidth
+                ),
+                (pokemonEntity -> pokemonEntity.getPokemon().isWild() && pokemonEntity.getPokemon().getForm().showdownId().equals(name))
+        );
+    }
+
+    abstract public FoundPokemon select(List<PokemonEntity> entities);
+}
