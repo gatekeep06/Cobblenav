9508fb9c571b7a05a6fc93fa39e9419a87750dd5
gatekeep06 <mvmetacontent@gmail.com>
2024-06-08 08:43:06 +0300

    add ClosestPokemonFinder

---
+++ src/main/java/com/metacontent/cobblenav/util/finder/ClosestPokemonFinder.java
@@ -0,0 +1,33 @@
+package com.metacontent.cobblenav.util.finder;
+
+import com.cobblemon.mod.common.entity.pokemon.PokemonEntity;
+import com.metacontent.cobblenav.Cobblenav;
+import com.metacontent.cobblenav.util.FoundPokemon;
+import net.minecraft.entity.ai.TargetPredicate;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class ClosestPokemonFinder extends PokemonFinder {
+    public ClosestPokemonFinder(PlayerEntity player, ServerWorld world) {
+        super(player, world);
+    }
+
+    @Nullable
+    @Override
+    public FoundPokemon select(List<PokemonEntity> entities) {
+        PokemonEntity closestEntity = world.getClosestEntity(
+                entities, TargetPredicate.DEFAULT, player,
+                Cobblenav.CONFIG.findingAreaWidth,
+                Cobblenav.CONFIG.findingAreaHeight,
+                Cobblenav.CONFIG.findingAreaWidth
+        );
+        if (closestEntity != null) {
+            return FoundPokemon.createOf(closestEntity);
+        }
+
+        return null;
+    }
+}
