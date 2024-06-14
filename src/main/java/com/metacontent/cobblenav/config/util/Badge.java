c6b13a4977fca51479f91d3939a2a650a152eca6
gatekeep06 <mvmetacontent@gmail.com>
2024-06-14 18:12:36 +0300

    override equals method
    for a more proper realization of the badge set

--- src/main/java/com/metacontent/cobblenav/config/util/Badge.java
+++ src/main/java/com/metacontent/cobblenav/config/util/Badge.java
@@ -3,6 +3,8 @@ package com.metacontent.cobblenav.config.util;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.server.network.ServerPlayerEntity;
 
+import java.util.Objects;
+
 public record Badge(
         String type,
         String permissionToGrant
@@ -12,5 +14,17 @@ public record Badge(
             return player.hasPermissionLevel(2);
         }
         return Permissions.check(player, permissionToGrant);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Badge badge)) return false;
+        return Objects.equals(type, badge.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type);
     }
 }
