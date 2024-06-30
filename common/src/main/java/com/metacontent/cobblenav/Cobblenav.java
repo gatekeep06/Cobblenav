fc71113cf1a7073d986535e26152c29ecdddc25f
gatekeep06 <mvmetacontent@gmail.com>
2024-06-30 20:38:25 +0300

    add logger

--- common/src/main/java/com/metacontent/cobblenav/Cobblenav.java
+++ common/src/main/java/com/metacontent/cobblenav/Cobblenav.java
@@ -1,7 +1,12 @@
 package com.metacontent.cobblenav;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Cobblenav {
 	public static final String ID = "cobblenav";
+	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+	
 
 	public static void init() {
 	}
