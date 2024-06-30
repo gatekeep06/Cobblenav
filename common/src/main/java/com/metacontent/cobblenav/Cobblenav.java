14752b84242ceffeeb34d35e61eed0751727abdf
gatekeep06 <mvmetacontent@gmail.com>
2024-06-30 23:32:56 +0300

    transfer config

--- common/src/main/java/com/metacontent/cobblenav/Cobblenav.java
+++ common/src/main/java/com/metacontent/cobblenav/Cobblenav.java
@@ -1,5 +1,6 @@
 package com.metacontent.cobblenav;
 
+import com.metacontent.cobblenav.config.CobblenavConfig;
 import com.metacontent.cobblenav.item.CobblenavItems;
 import com.metacontent.cobblenav.permission.PermissionHandler;
 import org.slf4j.Logger;
@@ -8,8 +9,9 @@ import org.slf4j.LoggerFactory;
 public class Cobblenav {
 	public static final String ID = "cobblenav";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
-	public static final String CONFIG_PATH = "/" + ID + "/cobblenav-config.json";
+	public static final String CONFIG_PATH = "config/" + ID + "/cobblenav-config.json";
     public static final String CLIENT_CONFIG_PATH = "config/" + ID + "/client-cobblenav-config.json";
+	public static final CobblenavConfig CONFIG = CobblenavConfig.init();
 	public static final PermissionHandler PERMISSIONS = new PermissionHandler();
 
     public static void init() {
---
+++ common/src/main/java/com/metacontent/cobblenav/config/CobblenavConfig.java
@@ -0,0 +1,163 @@
+package com.metacontent.cobblenav.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.metacontent.cobblenav.Cobblenav;
+import com.metacontent.cobblenav.config.util.Badges;
+import com.metacontent.cobblenav.config.util.PokemonFeatureWeights;
+import com.metacontent.cobblenav.config.util.TrainerContactInfo;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+
+public class CobblenavConfig {
+    public final boolean useCobbledexIntegration;
+    public final boolean useCounterIntegration;
+    public final boolean onlySeenPokemonMode;
+    public final boolean useCobblemonTrainersIntegration;
+    public final Badges badges;
+    public final int checkSpawnWidth;
+    public final int checkSpawnHeight;
+    public final int findingAreaWidth;
+    public final int findingAreaHeight;
+    public final PokemonFeatureWeights pokemonFeatureWeights;
+    public final List<String> ignoredLabels;
+    public final List<String> hiddenPokemon;
+    public final int titleCommandsPermissionLevel;
+    public final Map<String, TrainerContactInfo> trainerContactInfo;
+    public final List<Integer> customPokenavPredicates;
+    public final CounterIntegrationConfig counterIntegrationConfig;
+    public final boolean enableDebugMode;
+
+    private CobblenavConfig(
+            boolean useCobbledexIntegration,
+            boolean useCounterIntegration,
+            boolean onlySeenPokemonMode,
+            boolean useCobblemonTrainersIntegration,
+            Badges badges,
+            int checkSpawnWidth,
+            int checkSpawnHeight,
+            int findingAreaWidth,
+            int findingAreaHeight,
+            PokemonFeatureWeights pokemonFeatureWeights,
+            List<String> ignoredLabels,
+            List<String> hiddenPokemon,
+            int titleCommandsPermissionLevel,
+            Map<String, TrainerContactInfo> trainerContactInfo, List<Integer> customPokenavPredicates,
+            CounterIntegrationConfig counterIntegrationConfig,
+            boolean enableDebugMode
+    ) {
+        this.useCobbledexIntegration = useCobbledexIntegration;
+        this.useCounterIntegration = useCounterIntegration;
+        this.onlySeenPokemonMode = onlySeenPokemonMode;
+        this.useCobblemonTrainersIntegration = useCobblemonTrainersIntegration;
+        this.badges = badges;
+        this.checkSpawnWidth = checkSpawnWidth;
+        this.checkSpawnHeight = checkSpawnHeight;
+        this.findingAreaWidth = findingAreaWidth;
+        this.findingAreaHeight = findingAreaHeight;
+        this.pokemonFeatureWeights = pokemonFeatureWeights;
+        this.ignoredLabels = ignoredLabels;
+        this.hiddenPokemon = hiddenPokemon;
+        this.titleCommandsPermissionLevel = titleCommandsPermissionLevel;
+        this.trainerContactInfo = trainerContactInfo;
+        this.customPokenavPredicates = customPokenavPredicates;
+        this.counterIntegrationConfig = counterIntegrationConfig;
+        this.enableDebugMode = enableDebugMode;
+    }
+
+    private CobblenavConfig() {
+        this(false, false, false, false, new Badges(), -1, -1, 100, 100, PokemonFeatureWeights.BASE_WEIGHTS, List.of("not_modeled"), List.of(), 2, Map.of(), List.of(0), new CounterIntegrationConfig(), false);
+    }
+
+    public static CobblenavConfig init() {
+        Cobblenav.LOGGER.info("Initializing " + Cobblenav.ID + " config");
+        CobblenavConfig config;
+        Gson gson = (new GsonBuilder()).disableHtmlEscaping().setPrettyPrinting().create();
+        File file = new File(Cobblenav.CONFIG_PATH);
+        file.getParentFile().mkdirs();
+        if (file.exists()) {
+            try (FileReader fileReader = new FileReader(file)) {
+                config = gson.fromJson(fileReader, CobblenavConfig.class);
+            }
+            catch (Throwable throwable) {
+                Cobblenav.LOGGER.error(throwable.getMessage(), throwable);
+                config = new CobblenavConfig();
+            }
+        }
+        else {
+            config = new CobblenavConfig();
+        }
+
+        try (PrintWriter printWriter = new PrintWriter(file)) {
+            gson.toJson(config, printWriter);
+        }
+        catch (Throwable throwable) {
+            Cobblenav.LOGGER.error(throwable.getMessage(), throwable);
+        }
+
+        return config;
+    }
+
+    @Override
+    public String toString() {
+        return "CobblenavConfig{" +
+                "useCounterIntegration=" + useCounterIntegration +
+                ", useCobblemonTrainersIntegration=" + useCobblemonTrainersIntegration +
+                ", badges=" + badges +
+                ", checkSpawnWidth=" + checkSpawnWidth +
+                ", checkSpawnHeight=" + checkSpawnHeight +
+                ", findingAreaWidth=" + findingAreaWidth +
+                ", findingAreaHeight=" + findingAreaHeight +
+                ", pokemonFeatureWeights=" + pokemonFeatureWeights +
+                ", ignoredLabels=" + ignoredLabels +
+                ", hiddenPokemon=" + hiddenPokemon +
+                ", titleCommandsPermissionLevel=" + titleCommandsPermissionLevel +
+                ", customPokenavPredicates=" + customPokenavPredicates +
+                ", counterIntegrationConfig=" + counterIntegrationConfig +
+                ", enableDebugMode=" + enableDebugMode +
+                '}';
+    }
+
+    public static class CounterIntegrationConfig {
+        public final int levelOneStreak;
+        public final int levelTwoStreak;
+        public final int levelThreeStreak;
+        public final int levelFourStreak;
+        public final int levelZeroEggMoveChance;
+        public final int levelOneEggMoveChance;
+        public final int levelTwoEggMoveChance;
+        public final int levelThreeEggMoveChance;
+        public final int levelFourEggMoveChance;
+
+        private CounterIntegrationConfig(
+                int levelOneStreak,
+                int levelTwoStreak,
+                int levelThreeStreak,
+                int levelFourStreak,
+                int levelZeroEggMoveChance,
+                int levelOneEggMoveChance,
+                int levelTwoEggMoveChance,
+                int levelThreeEggMoveChance,
+                int levelFourEggMoveChance
+        ) {
+            this.levelOneStreak = levelOneStreak;
+            this.levelTwoStreak = levelTwoStreak;
+            this.levelThreeStreak = levelThreeStreak;
+            this.levelFourStreak = levelFourStreak;
+            this.levelZeroEggMoveChance = levelZeroEggMoveChance;
+            this.levelOneEggMoveChance = levelOneEggMoveChance;
+            this.levelTwoEggMoveChance = levelTwoEggMoveChance;
+            this.levelThreeEggMoveChance = levelThreeEggMoveChance;
+            this.levelFourEggMoveChance = levelFourEggMoveChance;
+        }
+
+        private CounterIntegrationConfig() {
+            this(5, 10, 20, 30, 0, 21, 46, 58, 65);
+        }
+
+    }
+}
