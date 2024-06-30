e460fecc468ac28bba5d2fa3194b2d9ac03b1347
gatekeep06 <mvmetacontent@gmail.com>
2024-06-30 20:39:23 +0300

    add config path

--- common/src/main/java/com/metacontent/cobblenav/Cobblenav.java
+++ common/src/main/java/com/metacontent/cobblenav/Cobblenav.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public class Cobblenav {
 	public static final String ID = "cobblenav";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
-	
+	public static final String CONFIG_PATH = "/" + ID + "/cobblenav-config.json";
 
 	public static void init() {
 	}
